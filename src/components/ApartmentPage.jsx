87a78b3abd90bd4ecc7886f866ea541790bd58c3
Hugo Dewas <hugodewas@gmail.com>
2023-12-07 02:19:45 +0100

    Layout v1 ApartmentPage

--- src/components/ApartmentPage.jsx
+++ src/components/ApartmentPage.jsx
@@ -4,27 +4,43 @@ import "./ApartmentPage.scss";
 function ApartmentPage() {
   return (
     <div className="apartment__page">
-      <div>
+      <div className="apartment__banner--image">
         <img src="bg-apt-page.webp" alt="" />
       </div>
-      <div className="apartment__title">
-        <h1>Crazy loft on Canal Saint Martin</h1>
-        <h2>Paris, île de France</h2>
-        <p>Cozy</p>
-        <p>Canal</p>
-        <p>Paris 10</p>
-      </div>
-      <div className="apartment__landlord">
-        <h3>Alexandre Dumas</h3>
-        <div className="rating">
-          <span>☆</span>
-          <span>☆</span>
-          <span>☆</span>
-          <span>☆</span>
-          <span>☆</span>
+
+      <div className="apartment__header">
+        <div className="apartment__title">
+          <h1>Cozy loft on Canal Saint Martin</h1>
+          <h2>Paris, île de France</h2>
+          <div className="tags">
+            <p>Cozy</p>
+            <p>Canal</p>
+            <p>Paris 10</p>
+          </div>
         </div>
-        <div className="apartment__description">
-          <p>Description</p>
+        <div className="apartment__landlord">
+          <div className="apartment__landlord--header">
+            <div className="apartment__landlord--name">
+              <p className="apartment__landlord--firstname">Alexandre </p>
+              <p className="apartment__landlord--lastname"> Dumas</p>
+            </div>
+            <div className="apartment__landlord--avatar">
+              <img src="grey.webp" alt="" />
+            </div>
+          </div>
+          <div className="apartment__landlord--rating">
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+            <span>☆</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="apartment__details">
+        <div className="apartment__details--description">
+          <p className="apartment__details--title">Description</p>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
             nisi consectetur minima, provident debitis reiciendis eveniet nihil
@@ -34,8 +50,8 @@ function ApartmentPage() {
             voluptatum veniam, facilis nulla, nobis earum.
           </p>
         </div>
-        <div className="apartment__equipment">
-          <p>Équipements</p>
+        <div className="apartment__details--equipment">
+          <p className="apartment__details--title">Équipements</p>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
             nisi consectetur minima, provident debitis reiciendis eveniet nihil
