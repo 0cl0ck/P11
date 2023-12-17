5daba70d4fced1ccd524c90661786b2d635149b5
0cl0ck <hugodewas@gmail.com>
2023-12-18 00:41:00 +0100

    Dynamique titre et cover des cards du Grid

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./Card.scss";
 import { Link } from "react-router-dom";
 
-function Card() {
+function Card(props) {
+  const cardStyle = {
+    backgroundImage: `url(${props.cover})`,
+  };
+
   return (
     <>
-      <Link to="/ApartmentPage" className="card__link">
-        <span className="card__subtitle">Titre de la Location</span>
+      <Link to="/ApartmentPage" className="card__link" style={cardStyle}>
+        <span className="card__subtitle">{props.title}</span>
       </Link>
     </>
   );
--- src/components/LocationGrid.jsx
+++ src/components/LocationGrid.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "./LocationGrid.scss";
 import Card from "./Card.jsx";
+import { useState, useEffect } from "react";
 
 function LocationGrid() {
+  const [apartments, setApartments] = useState([]);
+  useEffect(fetchApartments, []);
+
+  function fetchApartments() {
+    fetch("./src/data.json")
+      .then((response) => response.json())
+      .then((data) => setApartments(data))
+      .catch((error) => console.error(error));
+  }
+
   return (
     <div className="location-grid">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {apartments.map((apartment) => (
+        <Card
+          key={apartment.id}
+          title={apartment.title}
+          cover={apartment.cover}
+        />
+      ))}
     </div>
   );
 }
