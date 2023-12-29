385f6710fa6ea8d2bfba4ddd882b99bd6aeac693
Hugo Dewas <hugodewas@gmail.com>
2023-12-29 01:22:33 +0100

    Ajout Pagination carousel + CSS

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,50 +3,67 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Carousel({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const showControls = images.length > 1; // Show controls only if there are more than 1 image
 
   const goToPrevious = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
+    if (showControls) {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+    }
   };
 
   const goToNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
+    if (showControls) {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }
   };
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key === "ArrowLeft") {
-        goToPrevious();
-      } else if (event.key === "ArrowRight") {
-        goToNext();
+      if (showControls) {
+        if (event.key === "ArrowLeft") {
+          goToPrevious();
+        } else if (event.key === "ArrowRight") {
+          goToNext();
+        }
       }
     };
 
     window.addEventListener("keydown", handleKeyDown);
 
-    // Nettoyage de l'écouteur d'événements
+    // Cleanup listener
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [goToNext, goToPrevious]);
+  }, [goToNext, goToPrevious, showControls]);
 
   return (
     <div className="carousel">
-      <FontAwesomeIcon
-        className="carousel__chevrons"
-        onClick={goToPrevious}
-        icon="chevron-left"
-      />
-      <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} />
-      <FontAwesomeIcon
-        className="carousel__chevrons"
-        onClick={goToNext}
-        icon="chevron-right"
-      />
+      {showControls && (
+        <FontAwesomeIcon
+          className="carousel__chevrons"
+          onClick={goToPrevious}
+          icon="chevron-left"
+        />
+      )}
+      <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      {showControls && (
+        <div className="carousel__pagination">{`${currentIndex + 1} / ${
+          images.length
+        }`}</div>
+      )}
+      {showControls && (
+        <FontAwesomeIcon
+          className="carousel__chevrons"
+          onClick={goToNext}
+          icon="chevron-right"
+        />
+      )}
     </div>
   );
 }
+
 export default Carousel;
