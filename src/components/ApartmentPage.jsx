a0531f73097dd6f15c8ffbdc57b5701a127aac97
Hugo Dewas <hugodewas@gmail.com>
2023-12-29 01:47:34 +0100

    Ajout gestion erreur id apartmentPage

--- src/components/ApartmentPage.jsx
+++ src/components/ApartmentPage.jsx
@@ -5,48 +5,57 @@ import ApartmentHeader from "./ApartmentHeader";
 import InformationSection from "./InformationSection";
 import Carousel from "./Carousel.jsx";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function ApartmentPage() {
   const [apartment, setApartment] = useState(null);
+  const navigate = useNavigate();
   let { id } = useParams();
   useEffect(() => {
     fetch("../src/data.json")
       .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
         return response.json();
       })
       .then((data) => {
         const foundApartment = data.find((apt) => apt.id === id);
-        setApartment(foundApartment);
+        if (foundApartment) {
+          setApartment(foundApartment);
+        } else {
+          navigate("/404", { replace: true });
+        }
       })
-      .catch((error) => console.error(error));
-  }, [id]);
+      .catch((error) => {
+        console.error(error);
+        navigate("/404", { replace: true });
+      });
+  }, [id, navigate]);
 
+  if (!apartment) {
+    return <div>Chargement...</div>;
+  }
   return (
     <div className="apartment__page">
-      {apartment ? (
-        <>
-          <Carousel images={apartment.pictures} />
-          <ApartmentHeader
-            title={apartment.title}
-            location={apartment.location}
-            tags={apartment.tags}
-            host={apartment.host}
-            rating={parseInt(apartment.rating)}
-          />
-          <div className="apartment__details">
-            <InformationSection
-              title="Description"
-              content={apartment.description}
-            />
-            <InformationSection
-              title="Équipements"
-              content={apartment.equipments}
-            />
-          </div>
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
+      <Carousel images={apartment.pictures} />
+      <ApartmentHeader
+        title={apartment.title}
+        location={apartment.location}
+        tags={apartment.tags}
+        host={apartment.host}
+        rating={parseInt(apartment.rating)}
+      />
+      <div className="apartment__details">
+        <InformationSection
+          title="Description"
+          content={apartment.description}
+        />
+        <InformationSection
+          title="Équipements"
+          content={apartment.equipments}
+        />
+      </div>
     </div>
   );
 }
